073fe271c16446aba97590821132cd0ca6ea6685
vijayraghavan sudeshkumar <vijay010498>
2022-01-13 09:59:21 +0530

    gg

--- appendix_linked_list.js
+++ appendix_linked_list.js
@@ -1,16 +1,16 @@
 // Array - double up the size - O(n)
 // Linked List - O(1) size
 
-const basket = ['apples', 'grapes', 'pears'];
+const basket = ["apples", "grapes", "pears"];
 
 //linked list : apples --> grapes --> pears
 
 // pointer -> reference to another place in the memory
-const obj1 = {a: true}
+const obj1 = { a: true };
 const obj2 = obj1; // direct memory access
-obj1.a = 'vijay';
-console.log('1', obj1)
-console.log('2', obj2)
+obj1.a = "vijay";
+console.log("1", obj1);
+console.log("2", obj2);
 
 // reverse a linked list
 // constraints
@@ -18,14 +18,13 @@ console.log('2', obj2)
 //3 o/p -> 3
 
 const reverseLinkedList = function (head) {
-    let prev = null;
-    let current = head;
-    while (current) {
-        let next = current.next;
-        current.next = prev;
-        prev = current;
-        current = next;
-    }
-    return prev;
-}
-
+  let prev = null;
+  let current = head;
+  while (current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  return prev;
+};
