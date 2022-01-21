18c3a9d0abc4672869aed0086415bec088317195
vijay <vijay010498>
2022-01-21 12:09:08 +0530

    appendix-stacks-queues

---
+++ appendix_stacks_queues.js
@@ -0,0 +1,19 @@
+// Stacks and queues -> Linear data structure
+
+//Stack -> LIFO
+//Queues -> FIFO
+
+//Stacks
+//LIFO
+//Lookup -> O(n)
+//pop -> O(1)
+//push -> O(1)
+//peek -> O
+// Both arrays and Linked list works good for stacks
+
+//Queues
+//lookup -> O(n)
+//enqueue -> O(1)
+//dequeue -> O(1)
+//peek -> O(1)
+// Linked list is best
