d1d9a1dc8fe7f93081f1ffe30fa59f2f993186e6
vijay <vijay010498>
2022-01-22 11:43:49 +0530

    done-gg"

---
+++ problems/Q12.Implement-Queue-using-stack.js
@@ -0,0 +1,7 @@
+// Implement the class Queue using stacks. The queue methods you need to implement
+// are enqueue, dequeue, peek, and empty
+
+// Verifying constraints
+// 1. Do the queue methods we have to implement need to perform at the same
+// complexity of a real queue?
+// Ans. No, but they should be as performant as possible
